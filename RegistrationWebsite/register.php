5fcdefa4a1ef26643b84192769b7a44699db9ae3
codebreakeragr8 <54989169+codebreakeragr8@users.noreply.github.com>
2020-05-24 20:16:04 +0530

    Add files via upload
    This project is made By Aman Gupta
    CSE
    IIIT Sonepat

---
+++ RegistrationWebsite/register.php
@@ -0,0 +1,20 @@
+<!-- By Aman Gupta @IIIT Sonepat -->
+<!DOCTYPE html>
+<!-- server end scripting -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+<h1>Congratulations! You have Successfully Registered.</h1>
+  <p class="details">Welcome <?php echo $_POST["name"]; ?>
+    <br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>
+Your contact no. is: <?php echo $_POST["contact"]; ?>
+<br>
+</p>
+
+</html>
